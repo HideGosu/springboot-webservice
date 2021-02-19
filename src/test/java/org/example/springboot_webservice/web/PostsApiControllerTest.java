c5a044e3bb5264b4645eede8e9bdadc7150e1eec
HideGosu <natxkdla@naver.com>
2021-02-19 23:48:42 +0900

    PostsSaveRequestDto 클래스 추가

---
+++ src/test/java/org/example/springboot_webservice/web/PostsApiControllerTest.java
@@ -0,0 +1,14 @@
+package org.example.springboot_webservice.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+}
